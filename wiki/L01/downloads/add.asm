94a05cbcbc814b5f4a497068467530af19baa68f
obijuan <juan@iearobotics.com>
2019-01-24 17:41:18 +0100

    L01: Program added

---
+++ wiki/L01/downloads/add.asm
@@ -0,0 +1,7 @@
+	.text
+	
+	#-- t0 = x0 + 1  (x0 es un registro que siempre está a 0)
+	addi t0, zero, 1
+	
+	#--- Al llegar aquí el simulador se detiene pero....
+	#--- ¡¡¡¡el procesador real NO!!!
