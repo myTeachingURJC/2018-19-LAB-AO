5d3536951be41e034561a06f62ff8a6910d71eb4
obijuan <juan@iearobotics.com>
2019-02-21 15:23:55 +0100

    L5: More images added

--- wiki/L05/examples/data-04.s
+++ wiki/L05/examples/data-04.s
@@ -1,5 +1,5 @@
-#-- Ejemplo de almacenamiento de 4 medias palabras
-#-- (numeros de 16 bits)
+#-- Ejemplo de copia de una media palabra a 
+#-- otra posicion de memoria
 
 	.data
 var1:	.half 0xCAFE
@@ -8,6 +8,14 @@ var3:	.half 0xFACE
 var4:	.half 0xB00C
 
 	.text
+	
+	# -- Leer la variable var 2
+	la t0,var2
+	lh t1, 0(t0)
+	
+	# -- Escribirla sobre la variable 4
+	la t0, var4
+	sh t1, 0(t0)
 	
 	#--- Terminar
 	li a7, 10
